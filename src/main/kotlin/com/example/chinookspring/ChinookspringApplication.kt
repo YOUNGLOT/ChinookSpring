5a2910358a4cfec69ed23fa223d72b54b31e9b30
cplkimth <cplkimth@naver.com>
2019-11-22 14:45:15 +0900

    .

---
+++ src/main/kotlin/com/example/chinookspring/ChinookspringApplication.kt
@@ -0,0 +1,11 @@
+package com.example.chinookspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChinookspringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ChinookspringApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/chinookspring/ChinookspringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.chinookspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChinookspringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
